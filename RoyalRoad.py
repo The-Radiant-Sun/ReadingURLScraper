922e070bcb9f81b6af1f1d40355c3cf034dd1c6c
The-Radiant-Sun <65923244+The-Radiant-Sun@users.noreply.github.com>
2023-09-29 11:41:05 +1300

    V4 - Added console updates

--- RoyalRoad.py
+++ RoyalRoad.py
@@ -1,9 +1,10 @@
 import requests
+import threading
 from bs4 import BeautifulSoup
 
 
 class RoyalRoad:
-    def __init__(self, pageLimit):
+    def __init__(self, pageLimit, printPool):
         search = [
             'https://www.royalroad.com/fictions/complete',
             'https://www.royalroad.com/fictions/best-rated',
@@ -11,21 +12,40 @@ class RoyalRoad:
             'https://www.royalroad.com/fictions/rising-stars'
         ]
 
-        self.website = 'https://www.royalroad.com'
-
+        self.websiteURL = 'https://www.royalroad.com'
         self.page = '?page='
+        self.name = "Royal Road"
 
-        self.search = [f'{searchItem}{self.page}{i}' for i in range(1, pageLimit + 1) for searchItem in search] #Add page count to search
+        self.urlList = []
 
+        self.printPool = printPool
 
-    def getURLs(self):
-        urlList = []
+        self.search = [f'{searchItem}{self.page}{i}' for searchItem in search for i in range(1, pageLimit + 1)]  # Add page count to search
+
+    def getURLs(self, websiteURLs):
+        threads = []
 
         for fictionList in self.search:
-            page = requests.get(fictionList)
-            soup = BeautifulSoup(page.content, 'html.parser')
+            thread = threading.Thread(target=self.scanURLs, args=[fictionList])  # Create thread for each page
+            threads.append(thread)
+            thread.start()
 
-            urlList.append([self.website + a['href'] for [a] in [entry.find_all('a', href=True) for entry in soup.find_all('h2', class_='fiction-title')]])
+        for thread in threads:
+            thread.join()
 
-        return [urls for urlSet in urlList for urls in urlSet] #Flatten list
+        websiteURLs.append([urls for urlSet in self.urlList for urls in urlSet])  # Flatten list
+
+    def scanURLs(self, webpage):
+        self.cprint(f"{self.name} - Started scan of {webpage}")
+
+        page = requests.get(webpage)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        self.urlList.append([self.websiteURL + a['href'] for [a] in [entry.find_all('a', href=True) for entry in soup.find_all('h2', class_='fiction-title')]])
+
+        self.cprint(f"{self.name} - Ended scan of {webpage}")
+
+    def cprint(self, text):
+        self.printPool.acquire()
+        print(text)
+        self.printPool.release()
 
--- main.py
+++ main.py
@@ -1,20 +1,44 @@
+import threading
 from RoyalRoad import RoyalRoad
 
-file = 'urls.txt' #Name of storage file
+file = 'urls.txt'  # Name of storage file
 websites = [RoyalRoad]
-pageLimit = 5 #Number of pages deep in each website to pull from
+pageLimit = 5  # Number of pages deep in each website to pull from
+
+printPool = threading.Semaphore()
+
+
+def cprint(sema, text):
+    sema.acquire()
+    print(text)
+    sema.release()
 
 
 def main():
-    storage = open(file, mode='w', errors='replace') #Open or create storage file
-    storage.write('\n'.join(list(set(getURLs())))) #Remove duplicates through sets and write to storage
+    storage = open(file, mode='w', errors='replace')  # Open or create storage file
+    storage.write('\n'.join(list(set(getURLs()))))  # Remove duplicates through sets and write to storage
 
 
 def getURLs():
+    threads = []
     urlList = []
+
     for website in websites:
-        urlList.append(website(pageLimit).getURLs()) #Retrieve URLs
-    return [urls for urlSet in urlList for urls in urlSet] #Flatten list
+        thread = threading.Thread(target=scanWebsite, args=[website, urlList, printPool])  # Create thread for each website
+        threads.append(thread)
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+
+    return [urls for urlSet in urlList for urls in urlSet]  # Flatten list
+
+
+def scanWebsite(website, urlList, sema):
+    website = website(pageLimit, sema)
+    cprint(sema, f"Started scan of {website.name}")
+    website.getURLs(urlList)  # Retrieve URLs
+    cprint(sema, f"Ended scan of {website.name}")
 
 
 if __name__ == '__main__':
